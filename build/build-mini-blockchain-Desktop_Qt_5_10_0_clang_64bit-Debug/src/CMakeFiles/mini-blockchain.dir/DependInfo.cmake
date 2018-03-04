6bba8fc4fff7e3c0ded0a2b1da17850de305b09b
younux <benhoumich.younes@gmail.com>
2018-03-04 16:09:12 +0100

    Init project

---
+++ build/build-mini-blockchain-Desktop_Qt_5_10_0_clang_64bit-Debug/src/CMakeFiles/mini-blockchain.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/younes/Projects/mini-blockchain/src/main.cpp" "/Users/younes/Projects/mini-blockchain/build/build-mini-blockchain-Desktop_Qt_5_10_0_clang_64bit-Debug/src/CMakeFiles/mini-blockchain.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../.././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/build-mini-blockchain-Desktop_Qt_5_10_0_clang_64bit-Debug/src/CMakeFiles/mini-blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini-blockchain.dir/main.cpp.o"
+  "mini-blockchain.pdb"
+  "mini-blockchain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini-blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
